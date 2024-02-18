7dd7008013e62805524044082a703679ed15f18e
izalfront <rizalhajakubastarus@gmail.com>
2024-02-18 18:30:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/temenin_1/BottomNavigationBar.kt
@@ -0,0 +1,33 @@
+package com.example.temenin_1
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Wallet
+import androidx.compose.runtime.Composable
+import com.example.temenin_1.data.BottomNavigation
+
+val items = listOf(
+    BottomNavigation(
+        title = "Home",
+        icon = Icons.Rounded.Home
+    ),
+    BottomNavigation(
+        title = "Wallet",
+        icon = Icons.Rounded.Wallet
+    ),
+    BottomNavigation(
+        title = "Notifications",
+        icon = Icons.Rounded.Notifications
+    ),
+    BottomNavigation(
+        title = "Account",
+        icon = Icons.Rounded.Person
+    )
+)
+
+@Composable
+fun BottomNavigationBar(){
+
+}
